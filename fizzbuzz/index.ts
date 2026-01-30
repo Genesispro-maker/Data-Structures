666e706042ddf139afd3f85a56f5d61e417aa917
Genesispro-maker <genesisprog894@gmail.com>
2026-01-30 11:25:49 -0800

    Update index.ts

--- fizzbuzz/index.ts
+++ fizzbuzz/index.ts
@@ -2,8 +2,19 @@ const fizzbuzz = (n: number) : number | string => {
     if(typeof n !== "number"){
         throw new Error("Not a number")
     }
+   
 
-    switch(n){
-        case n % 
+    if(n % 3 === 0){
+        return "fizz"
     }
+    else if(n % 5 === 0){
+        return "buzz"
+    }
+    else if(n % 3 === 0 && n % 5 === 0){
+        return "fizzbuzz"
+    }
+
+
+    return n
 }
+fizzbuzz(5)
